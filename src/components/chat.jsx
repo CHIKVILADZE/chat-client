8d761a48a2b3b376e9a991866c4f03a73bd5fded
Giorgi Chikviladze <chikviladze555@yahoo.com>
2023-08-17 13:45:28 +0400

    some changes

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -8,7 +8,7 @@ import { Timeline } from 'primereact/timeline';
 import { io } from 'socket.io-client';
 import TagsInput from './TagsInput';
 
-const socket = io('https://chat-server-l3ck.onrender.com');
+const socket = io('https://chat-server-l3ck.onrender.com/');
 
 function Chat({ setMessages, messages }) {
   const [author, setAuthor] = useState('');
